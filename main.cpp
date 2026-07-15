93dc6d38da07fe4db2f1c9ba447fd71f49fb3ae6
Luis <luiscornieles30@gmail.com>
2026-07-14 20:31:18 -0400

    Martes (Primer día)

--- main.cpp
+++ main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 // Programa hecho por: Luis Cornieles, Andres Dominguez y Alfredo Urbaez
 int main() {
-    char pregunta;
-    do
+    char pregunta; std::string nombre; int codigo; float sueldo;
+ do
     {
+        std::cout << "Ingrese su nombre:";
+        std::cin >> nombre;
+        std::cout << "Ingrese su codigo:";
+        std::cin >> codigo;
+        std::cout <<"Ingrese su sueldo:";
+        std::cin >> sueldo;
+
+        
+        
+        
+        
+        
+        
         std::cout << "¿Desea continuar? (s/n): ";
         std::cin >> pregunta;
         std::cin.ignore(100000000, '\n');
     } while (pregunta != 'n' && pregunta != 'N');
 
-    
+
     return 0;
 }
