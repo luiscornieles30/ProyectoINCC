d3a44693a325983d562760c81a4a0461eb3bb474
Luis <luiscornieles30@gmail.com>
2026-07-17 16:38:29 -0400

    50% del trabajo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 // Programa hecho por: Luis Cornieles, Andres Dominguez y Alfredo Urbaez
 int main() {
-    char pregunta; std::string nombre; int codigo; float sueldo; float aumento1; float aumento2; float aumento3;
+    char pregunta; int empleados; std::string nombre; int codigo; float sueldo; float aumento1; float aumento2; float aumento3; float m_aumento; float m_aumento2; float m_aumento3; std::string nombrem_aumento3; std::string nombrem_aumento2; std::string nombrem_aumento; 
+m_aumento = 0;
+m_aumento2 = 0;
+m_aumento3 = 0;
+empleados = 0;
+
+
+std::cout << "Programa para calcular el aumento de sueldo de los empleados" << std::endl;
  do
     {
         std::cout << "Ingrese su nombre:";
@@ -10,25 +17,32 @@ int main() {
         std::cin >> codigo;
         std::cout <<"Ingrese su sueldo:";
         std::cin >> sueldo;
+    if (sueldo <= 100000) {
+        aumento1 = sueldo * 1.30;
+        if (aumento1 > m_aumento) {
+            m_aumento = aumento1;
+            nombrem_aumento = nombre;
+            } 
+        }
+    if (sueldo > 100000 && sueldo <= 200000) {
+        aumento2 = sueldo + 50000;
+        if (aumento2 > m_aumento2) {
+            m_aumento2 = aumento2;
+            nombrem_aumento2 = nombre;
+            }   
+        }
+    if (sueldo > 200000) {  
+        aumento3 = sueldo * 1.20;
+        if (aumento3 > m_aumento3) {
+            m_aumento3 = aumento3;
+            nombrem_aumento3 = nombre;
+            }
+        }
+    empleados = empleados + 1;
 
-        
-        
-        
-        
-        
-        
         std::cout << "¿Desea continuar? (s/n): ";
         std::cin >> pregunta;
         std::cin.ignore(100000000, '\n');
     } while (pregunta != 'n' && pregunta != 'N');
-if (sueldo <= 100000) {
-    aumento1 = sueldo * 1.30;   
-    }
-if (sueldo > 100000 && sueldo <= 200000) {
-   aumento2 = sueldo + 50000;   
-    }
-if (sueldo > 200000) {  
-    aumento3 = sueldo * 1.20;   
-    }
     return 0;
 }
