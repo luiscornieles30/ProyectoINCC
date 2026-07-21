663781dcc34d3aaafcf4eee152b0de052af1cbd4
Luis <luiscornieles30@gmail.com>
2026-07-21 17:11:29 -0400

    Bugs Resueltos 11/11

--- Proyecto1_AndresDominguez_LuisCornieles_AlfredoUrbaez.cpp
+++ Proyecto1_AndresDominguez_LuisCornieles_AlfredoUrbaez.cpp
@@ -3,7 +3,7 @@
 // Programa hecho por: Luis Cornieles, Andres Dominguez y Alfredo Urbaez, estudiantes de ingeniería de la Universidad Fermin Toro, Nucleo Lara-Cabudare.
 //Cambio de Nombre.
 int main() {
-    char pregunta; float sueldoantes; float sueldodespues;  float promedio1; float promedio2; float promedio3; float prom_aumento1; float prom_aumento2; float prom_aumento3; int empleados; std::string nombre; int codigo; float sueldo; float aumento1; float aumento2; float aumento3; float m_aumento; float m_aumento2; float m_aumento3; std::string nombrem_aumento3; std::string nombrem_aumento2; std::string nombrem_aumento; float empleado1; float empleado2; float empleado3;
+    char pregunta; float sueldoantes; float sueldoluego; float aumentom1; float aumentom2; float aumentom3; float sueldodespues;  float promedio1; float promedio2; float promedio3; float prom_aumento1; float prom_aumento2; float prom_aumento3; int empleados; std::string nombre; int codigo; float sueldo; float aumento1; float aumento2; float aumento3; float m_aumento; float m_aumento2; float m_aumento3; std::string nombrem_aumento3; std::string nombrem_aumento2; std::string nombrem_aumento; float empleado1; float empleado2; float empleado3;
 m_aumento = 0;
 m_aumento2 = 0;
 m_aumento3 = 0;
@@ -19,15 +19,16 @@ promedio3 = 0;
 prom_aumento1 = 0;
 prom_aumento2 = 0;  
 prom_aumento3 = 0;
-
-
-
+nombrem_aumento = "(No hay empleados en este rango)";
+nombrem_aumento2 = "(No hay empleados en este rango)";
+nombrem_aumento3 = "(No hay empleados en este rango)";
 
 
 std::cout << std::fixed << std::setprecision(2);
 std::cout << "\033[1;34m" << "<<<<<<<<<<<<<<<<<<<<   Programa para calcular el aumento de sueldo de los empleados   >>>>>>>>>>>>>>>>>>>>" << "\033[0m" << std::endl;
  do
-    {   std::cout << "\033[36m" << "<<< Escriba 'Cancelar' o 'cancelar' cuando le pida su nombre y/o '0' cuando le pida algún dato numerico para finalizar el programa >>>" << "\033[0m" << std::endl;
+    {   sueldoluego = 0;
+        std::cout << "\033[36m" << "<<< Escriba 'Cancelar' o 'cancelar' cuando le pida su nombre y/o '0' cuando le pida algún dato numerico para finalizar el programa >>>" << "\033[0m" << std::endl;
         std::cout << "\033[90m" << "Ingrese su nombre:" << "\033[0m";
         std::cin >> nombre;
         if (nombre =="Cancelar" || nombre =="cancelar") {
@@ -64,6 +65,7 @@ std::cout << "\033[1;34m" << "<<<<<<<<<<<<<<<<<<<<   Programa para calcular el a
         }
     if (sueldo <= 100000 && sueldo > 0) {
         aumento1 = sueldo * 1.30;
+        aumentom1 = aumento1;
         empleado1 = empleado1 + 1;
         if (aumento1 > m_aumento) {
             m_aumento = aumento1;
@@ -72,10 +74,12 @@ std::cout << "\033[1;34m" << "<<<<<<<<<<<<<<<<<<<<   Programa para calcular el a
         prom_aumento1 = prom_aumento1 + aumento1;
         promedio1 = prom_aumento1 / empleado1;
         sueldodespues = sueldodespues + aumento1;
+        sueldoluego = aumento1;
 
         }
     if (sueldo > 100000 && sueldo <= 200000) {
         aumento2 = sueldo + 50000;
+        aumentom2 = aumento2;
         if (aumento2 > m_aumento2) {
             m_aumento2 = aumento2;
             nombrem_aumento2 = nombre;
@@ -84,9 +88,11 @@ std::cout << "\033[1;34m" << "<<<<<<<<<<<<<<<<<<<<   Programa para calcular el a
         prom_aumento2 = prom_aumento2 + aumento2;
         promedio2 = prom_aumento2 / empleado2;
         sueldodespues = sueldodespues + aumento2;
+        sueldoluego =aumento2;
         }
     if (sueldo > 200000) {  
         aumento3 = sueldo * 1.20;
+        aumentom3 = aumento3;
         if (aumento3 > m_aumento3) {
             m_aumento3 = aumento3;
             nombrem_aumento3 = nombre;
@@ -95,16 +101,17 @@ std::cout << "\033[1;34m" << "<<<<<<<<<<<<<<<<<<<<   Programa para calcular el a
         prom_aumento3 = prom_aumento3 + aumento3;
         promedio3 = prom_aumento3 / empleado3;
         sueldodespues = sueldodespues + aumento3;
+        sueldoluego = aumento3;
         }
     empleados = empleados + 1;
     sueldoantes = sueldoantes + sueldo;
-    std::cout << "El Empleado " << nombre << " con codigo " << codigo << " tiene un sueldo de: " << sueldo << " y su aumento es de: " << sueldodespues << std::endl;
+    std::cout << "El Empleado " << nombre << " con codigo " << codigo << " tiene un sueldo de: " << sueldo << " y su aumento es de: " << sueldoluego << std::endl;
         std::cout << "\033[4;32m" << "¿Desea continuar? (s/n): " << "\033[0m";
         std::cin >> pregunta;
         std::cin.ignore(100000000, '\n');
     if (pregunta != 's' && pregunta != 'S') {
-        std::cout << "\033[4;31m" << "<< Coloque un caracter valido para seguir la ejecución del programa o finalizar y recibir los datos finales >>" << "\033[0m" << std::endl;
-        continue;
+        std::cout << "\033[4;31m" << "<< Coloque un caracter valido acorde a las indicaciones la proxima vez >>" << "\033[0m" << std::endl;
+        break;
     }
     } while ((pregunta != 'n' && pregunta != 'N'));
 if (pregunta == 'n' || pregunta == 'N') {
@@ -117,6 +124,14 @@ if (pregunta == 'n' || pregunta == 'N') {
   std::cout << "\033[92m" << "El valor total de los sueldos antes del aumento es: " << sueldoantes << std::endl;
   std::cout << "\033[94m" << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << "\033[0m" << std::endl;
   std::cout << "\033[92m" << "El valor total de los sueldos despues del aumento es: " << sueldodespues << std::endl;
+  std::cout << "\033[94m" << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << "\033[0m" << std::endl;
+  std::cout << "\033[92m" << "La cantidad total de empleados es: " << empleados << "\033[0m" << std::endl;
+  std::cout << "\033[94m" << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << "\033[0m" << std::endl;
+  std::cout << "\033[92m" << "El promedio de aumento en el rango de 0 a 100000 es: " << promedio1 << std::endl;
+  std::cout << "\033[94m" << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << "\033[0m" << std::endl;
+  std::cout << "\033[92m" << "El promedio de aumento en el rango de 100000 a 200000 es: " << promedio2 << std::endl;
+  std::cout << "\033[94m" << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << "\033[0m" << std::endl;
+  std::cout << "\033[92m" << "El promedio de aumento en el rango de 200000 a mas es: " << promedio3 << std::endl;
   std::cout << "\033[94m" << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << "\033[0m" << std::endl;
 }
 // Fin del Programa, gracias por su atención a la explicación del mismo.
